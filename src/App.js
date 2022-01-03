342e8de08d5eb1489adc23acd98fa2d59aadda25
fasbrothers <nigmatovsanjar00@gmail.com>
2022-01-03 11:55:07 +0500

    collect all components in app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Articles from "./components/Articles";
+import Services from "./components/Services";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/articles" exact element={<Articles />} />
+        <Route path="/services" exact element={<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
